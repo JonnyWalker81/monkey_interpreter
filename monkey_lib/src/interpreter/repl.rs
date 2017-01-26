2e4045f58ba2a5361a7dfaf94f62890cc9e2e16d
Jonathan Rothberg <jrothberg@bluebeam.com>
2017-01-25 17:31:08 -0800

    Calc more

--- monkey_lib/src/interpreter/repl.rs
+++ monkey_lib/src/interpreter/repl.rs
@@ -72,6 +72,60 @@ impl Repl {
         } 
     }
 
+    // TODO: test
+    pub fn start_calc(stdin: &mut Stdin, stdout: &mut Stdout) {
+        let mut buffer = String::new();
+        let mut env = Arc::new(RefCell::new(Environment::new()));
+        let mut last_good_result = String::new();
+
+        loop {
+            // print!("{}", PROMPT);
+            stdout.flush().unwrap();
+            buffer.clear();
+            // stdin.read_line(&mut buffer);
+            let input = readline::readline(PROMPT);
+             match input {
+                 Ok(string) => {
+                     readline::add_history(string.as_str());
+                     buffer = string;
+                 },
+                Err(e) => break,
+            }
+
+            let mut l = Lexer::new(buffer.clone());
+            let mut p = Parser::new(l);
+
+
+            let program = p.parse_program();
+            if let Some(pro) = program {
+                if p.get_errors().len() != 0 {
+                        write!(stdout, "{}", last_good_result);
+                        write!(stdout, "\n");
+                    // Repl::print_parse_errors(stdout, &p.get_errors());
+                    continue;
+                }
+
+                let evaluated = Evaluator::eval_program(&pro, &mut env);
+                match evaluated {
+                    ObjectType::Null => {
+                        write!(stdout, "{}", last_good_result);
+                        write!(stdout, "\n");
+                    },
+                    ObjectType::Error(..) => {
+                        write!(stdout, "{}", last_good_result);
+                        write!(stdout, "\n");
+                    },
+                    _ => {
+                        last_good_result.clear();
+                        last_good_result = format!("{}", evaluated);
+                        write!(stdout, "{}", evaluated);
+                        write!(stdout, "\n");
+                    }
+                }
+            }
+        }
+    }
+
     fn print_parse_errors(stdout: &mut Stdout, errors: &Vec<String>) {
         write!(stdout, "{}", MONKEY_FACE);
         write!(stdout, "Whoops! We ran into some monkey business here!\n");
